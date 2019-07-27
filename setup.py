59426ca5c9739159f2b260f0ca8b8e3b45178487
Thomas Waldmann <tw@waldmann-edv.de>
2019-07-27 16:59:07 +0200

    fix list of qualifiers (pypi metadata)

--- setup.py
+++ setup.py
@@ -45,10 +45,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python 2.7',
-        'Programming Language :: Python 3.5',
-        'Programming Language :: Python 3.6',
-        'Programming Language :: Python 3.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
